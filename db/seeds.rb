bf113399176f3f8241b497020d08fc6c4d18dc59
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-19 12:07:19 -0400

    Schema and db seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Scenario.delete_all
+puts 'Creating Scenarios'
+2.times do |i|
+  Scenario.create!(title: Faker::Lorem.words(rand(1..3)).join(' '), is_enabled?: true)
+end
+
+Message.delete_all
+puts 'Creating Messages'
+5.times do |i|
+  Message.create!(trigger: Faker::Lorem.word, content: Faker::Lorem.sentence)
+end
+
+User.delete_all
+puts 'Creating Users'
+joe = User.create!(email: 'joe@example.com', password: 'password')
+mandy = User.create!(email: 'mandy@example.com', password: 'password')
+fig = User.create!(email: 'fig@example.com', password: 'password')
+
+3.times do |i|
+  joe.messages.create!(trigger: "joe_message_#{i}", content: Faker::Lorem.sentence)
+end
+
+2.times do |i|
+  mandy.messages.create!(trigger: "mandy_message_#{i}", content: Faker::Lorem.sentence)
+end
+
+2.times do |i|
+  fig.messages.create!(trigger: "fig_message_#{i}", content: Faker::Lorem.sentence)
+end
+
