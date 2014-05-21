1d3f09d5a69e6a23476a11c500e7f1d3397999cd
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-21 16:54:41 -0400

    Fixed typo w/ instance vs. local variable in twi controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -23,8 +23,8 @@ class TwilioController < ApplicationController
   end
 
   def process_sms
-    @from = User.where(phone: params[:From]).first
-    @call_message = from.messages.where(trigger: params[:Body]).first
+    @from = User.where(phone: params["From"]).first
+    @call_message = @from.messages.where(trigger: params["Body"]).first
     @call = @client.account.calls.create({
         :to => @from, # To escapee's number
         :from => @app_number, # From app's Twilio number
