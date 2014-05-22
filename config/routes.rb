5aa973b7606903793eb021d8fa354c511a8c1758
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-22 11:35:38 -0400

    Routing authenticated users to dashboard, non to static homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'messages#index'
+  # Authenticated users to go their dashboard
+  authenticated do
+    root :to => 'home#dashboard', as: :authenticated
+  end
+
+  # Non-authenticated users to go static homepage
+  root :to => 'home#index'
+
+  resources :home, only: [:index]
+  get 'howitworks' => 'home#howitworks'
+
   resources :messages
 
   # Twilio API routes
   get 'history' => 'twilio#history'
-  get 'call' => 'twilio#call'
   get 'twilio/process_sms' => 'twilio#process_sms'
   get 'twilio/call_handler' => 'twilio#call_handler'
 
