6f1394963e7f4e4f154ac395e3a9d2a0b7eee781
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-22 10:48:47 -0400

    Removed Scenario variable from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Scenario.delete_all
-
 Message.delete_all
 puts 'Creating Messages'
 m1 = Message.create!(trigger: 'figmergency', content: 'The cat is vomiting all over the living room. Please advise.', voice: 'woman', language: 'en')
